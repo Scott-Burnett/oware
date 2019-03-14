868369a9f83d9d0232f6f17f566b7f0e9d25ac3c
Scott Burnett <G14B4098@ict.ru.ac.za>
2019-03-14 16:01:19 +0200

    More work done

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -1,5 +1,7 @@
 ﻿module Oware
 
+open System.IO
+
 type StartingPosition =
     | South
     | North
@@ -19,8 +21,7 @@ type board = {
     f' : int
     npoints : int
     spoints : int
-    turn : StartingPosition 
-} 
+    turn : StartingPosition } 
     
 let getSeeds n board = 
     match n with 
@@ -38,13 +39,26 @@ let getSeeds n board =
         |12 -> board.f'
         |_ -> failwith "Out of range"
 
-let useHouse n board = failwith "Not implemented"
+let useHouse n board = 
+    let rec go a b c d e f a' b' c' d' e' f' temp nummoves = 
+        match temp = 0 with 
+            |true -> 1
+            |false -> go (b + 1) c d e f a' b' c' d' e' f' a (temp - 1) nummoves
+    match n with 
+        |1 -> go 0 board.b 
+     
 
 let start position = {a = 4; b = 4; c = 4; d = 4; e = 4; f = 4; a' = 4; b' = 4; c' = 4; d' = 4; e' = 4; f' = 4; npoints = 0; spoints = 0; turn = position}
 
-let score board = failwith "Not implemented"
+let score board = (board.spoints, board.npoints)
 
-let gameState board = failwith "Not implemented"
+let gameState board = match board.npoints > 25, board.spoints > 25 with 
+    |true, true -> "Game ended in a draw"
+    |true, false -> "North won"
+    |false, true -> "South won"
+    |false, false -> match board.turn with 
+        |South -> "South's turn"
+        |North -> "North's turn"
 
 [<EntryPoint>]
 let main _ =
