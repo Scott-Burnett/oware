cdb8b8b7ed8883b64b3c40ad77a9f5253932e29f
FreeWillie03 <staplewm@gmail.com>
2019-03-21 17:21:10 +0200

    Need to finish useHouse

--- Oware/Oware/Program.fs
+++ Oware/Oware/Program.fs
@@ -4,11 +4,72 @@ type StartingPosition =
     | South
     | North
 
-let getSeeds n board = failwith "Not implemented"
+type board = {
+    a : int 
+    b : int 
+    c : int
+    d : int 
+    e : int
+    f : int 
+    a' : int
+    b' : int 
+    c' : int
+    d' : int 
+    e' : int
+    f' : int 
+}
 
-let useHouse n board = failwith "Not implemented"
+type points = 
+    | Spoints of int 
+    | Npoints of int
 
-let start position = failwith "Not implemented"
+
+let getSeeds n board = 
+    match n with 
+    | 1 -> board.a
+    | 2 -> board.b
+    | 3 -> board.c
+    | 4 -> board.d
+    | 5 -> board.e
+    | 6 -> board.f
+    | 7 -> board.a'
+    | 8 -> board.b'
+    | 9 -> board.c'
+    | 10 -> board.d'
+    | 11 -> board.e'
+    | 12 -> board.f'
+    | _ -> failwith "Getseeds, Out of range exception: n>12"
+
+//let houseToNum n =
+// match n with
+// | 1 -> 'a'
+// | 2 -> 'b'
+// | 3 -> 'c'
+// | 4 -> 'd'
+// | 5 -> 'e'
+// | 6 -> 'f'
+// | 7 -> 'a'
+// | 8 -> 'b'
+// | 9 -> 'c'
+// | 10 -> 'd'
+// | 11 -> 'e'
+// | 12 -> 'f'
+
+let useHouse n board = 
+    let SeedsToBeUsed = getSeeds n board
+    let rec bob move = 
+       match (move>0) with 
+       |true -> 
+        {a = 0; b = 5; c = 5; d = 5; e = 5; f = 4; a' = 4; b' = 4; c' = 4; d' = 4; e' = 4; f' = 4}//set selected house to 0, get the current house, set to 0, copy and update (add 1 to) (n+1)
+       | _ -> failwith "UseHouse, recursive part: move=<0"
+    match (n>0 && n<13) with
+    |true -> bob SeedsToBeUsed
+    |_ -> failwith "UseHouse, n<0 or n>12"
+    
+        
+
+
+let start position = {a = 4;b = 4; c = 4; d = 4; e = 4; f = 4; a' = 4; b' = 4; c' = 4; d' = 4; e' = 4; f' = 4}
 
 let score board = failwith "Not implemented"
 
