67694a56b85cf7553a604bd229cc76b48abf423a
Scott-Burnett <thebreakfastfactoryyay@gmail.com>
2019-03-25 18:26:43 +0200

    Decomposed UseHouse function, Standardised Functions

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -39,53 +39,76 @@ let getSeeds n board =
         |12 -> board.f'
         |_ -> failwith "Out of range"
 
+let houseBelongsToPlayer n board =
+    ((n > 0 && n < 7 && board.turn = South) || (n > 5 && n < 13 && board.turn = North))
+
+let houseIsNotEmpty n board = 
+    (getSeeds n board) > 0
+
 let pHasNoSeeds ({a = a; b = b; c = c; d = d; e = e; f = f; a' = a'; b' = b'; c' = c'; d' = d'; e' = e'; f' = f'; turn = turn } as board) =
     (a = 0 && b = 0 && c = 0 && d = 0 && e = 0 && f = 0 && turn = South) || (a' = 0 && b' = 0 && c' = 0 && d' = 0 && e' = 0 && f' = 0 && turn = North)
 
-let useHouse n ({a = a; b = b; c = c; d = d; e = e; f = f; a' = a'; b' = b'; c' = c'; d' = d'; e' = e'; f' = f'; turn = turn } as board) = 
-    let getboard a b c d e f a' b' c' d' e' f' spoints npoints turn = 
-        match turn = South with 
-            |true -> { a = a; b = b; c = c; d = d; e = e; f = f; a' = a'; b' = b'; c' = c'; d' = d'; e' = e'; f' = f'; spoints = spoints; npoints = npoints; turn = North }
-            |false -> { a = a; b = b; c = c; d = d; e = e; f = f; a' = a'; b' = b'; c' = c'; d' = d'; e' = e'; f' = f'; spoints = spoints; npoints = npoints; turn = South }
-    let rec reset a b c d e f a' b' c' d' e' f' sp np turn offset =
+let nextTurn ({turn = turn} as board) =
+    match turn with
+        |South -> {board with turn = North}
+        |North -> {board with turn = South}
+
+let getboard (a, b, c, d, e, f, a', b', c', d', e', f', spoints, npoints, turn, offset) =            
+    let rec reset a b c d e f a' b' c' d' e' f' spoints npoints turn offset =
         match offset = 0 with 
-            |true -> getboard a b c d e f a' b' c' d' e' f' sp np turn
-            |false -> reset f' a b c d e f a' b' c' d' e' sp np turn ((offset + 11) % 12)
-    let getpoints a b c d e f a' b' c' d' e' f' spoints npoints turn offset = 
-        let justInCaseBoard = reset a b c d e f a' b' c' d' e' f' spoints npoints turn offset
-        let rec inner a b c d e f a' b' c' d' e' f' spoints npoints turn offset =
-            match (a = 2 || a = 3), (offset >= 0 && offset <= 5 && turn = North), (offset >= 6 && offset <= 11 && turn = South) with
-                |true, true, false -> inner f' 0 b c d e f a' b' c' d' e' (spoints) (npoints + a) turn ((offset + 11) % 12)
-                |true, false, true -> inner f' 0 b c d e f a' b' c' d' e' (spoints + a) (npoints) turn ((offset + 11) % 12)
-                |_, _, _ -> let nextboard = reset a b c d e f a' b' c' d' e' f' spoints npoints turn offset
-                            match pHasNoSeeds nextboard with
-                                |true -> justInCaseBoard
-                                |false -> nextboard
-        inner a b c d e f a' b' c' d' e' f' spoints npoints turn offset
-    let rec sowseeds a b c d e f a' b' c' d' e' f' numseeds offset cycles = 
+            |true -> { a = a; b = b; c = c; d = d; e = e; f = f; a' = a'; b' = b'; c' = c'; d' = d'; e' = e'; f' = f'; spoints = spoints; npoints = npoints; turn = turn }
+            |false -> reset f' a b c d e f a' b' c' d' e' spoints npoints turn ((offset + 11) % 12)
+    reset a b c d e f a' b' c' d' e' f' spoints npoints turn offset
+
+let getpoints (a, b, c, d, e, f, a', b', c', d', e', f', spoints, npoints, turn, offset) = 
+    let justInCaseBoard = nextTurn (getboard (a, b, c, d, e, f, a', b', c', d', e', f', spoints, npoints, turn, offset))
+    let rec inner a b c d e f a' b' c' d' e' f' spoints npoints turn offset =
+        match (a = 2 || a = 3), (offset >= 0 && offset <= 5 && turn = North), (offset >= 6 && offset <= 11 && turn = South) with
+            |true, true, false -> inner f' 0 b c d e f a' b' c' d' e' spoints (npoints + a) turn ((offset + 11) % 12)
+            |true, false, true -> inner f' 0 b c d e f a' b' c' d' e' (spoints + a) npoints turn ((offset + 11) % 12)
+            |_, _, _ -> let nextboard = nextTurn (getboard (a, b, c, d, e, f, a', b', c', d', e', f', spoints, npoints, turn, offset))
+                        match pHasNoSeeds nextboard with
+                            |true -> justInCaseBoard
+                            |false -> nextboard
+    inner a b c d e f a' b' c' d' e' f' spoints npoints turn offset
+
+let rec sowseeds (a, b, c, d, e, f, a', b', c', d', e', f', spoints, npoints, turn, offset) numseeds cycles = 
+    let rec inner a b c d e f a' b' c' d' e' f' spoints npoints turn offset numseeds cycles =
         match numseeds = 1, (cycles = 0) with
-            |_, true -> sowseeds b c d e f a' b' c' d' e' f' a numseeds ((offset + 1) % 12) ((cycles + 1) % 12)
-            |true, false -> getpoints (a + 1) b c d e f a' b' c' d' e' f' board.spoints board.npoints board.turn (offset) 
-            |false, false -> sowseeds b c d e f a' b' c' d' e' f' (a + 1) (numseeds - 1) ((offset + 1) % 12) ((cycles + 1) % 12)
+            |_, true -> inner b c d e f a' b' c' d' e' f' a spoints npoints turn ((offset + 1) % 12) numseeds ((cycles + 1) % 12)
+            |false, false -> inner b c d e f a' b' c' d' e' f' (a + 1) spoints npoints turn ((offset + 1) % 12) (numseeds - 1) ((cycles + 1) % 12)
+            |true, false -> getpoints ((a + 1), b, c, d, e, f, a', b', c', d', e', f', spoints, npoints, turn, offset)        
+    inner a b c d e f a' b' c' d' e' f' spoints npoints turn offset numseeds cycles
+
+let noValidTurns ({a = a; b = b; c = c; d = d; e = e; f = f; a' = a'; b' = b'; c' = c'; d' = d'; e' = e'; f' = f'} as board) =
+    (a < 6 && b < 5 && c < 4 && d < 3 && e < 2 && f < 1 && a' < 6 && b' < 5 && c' < 4 && d' < 3 && e' < 2 && f' < 1)
+
+let cleanUp ({a = a; b = b; c = c; d = d; e = e; f = f; a' = a'; b' = b'; c' = c'; d' = d'; e' = e'; f' = f'; spoints = spoints; npoints = npoints; turn = turn} as board) =
+    {a = 0; b = 0; c = 0; d = 0; e = 0; f = 0; a' = 0; b' = 0; c' = 0; d' = 0; e' = 0; f' = 0; spoints = spoints + a + b + c + d + e + f; npoints = npoints + a' + b' + c' + d' + e' + f'; turn = turn }
+
+let useHouse n ({a = a; b = b; c = c; d = d; e = e; f = f; a' = a'; b' = b'; c' = c'; d' = d'; e' = e'; f' = f'; spoints = spoints; npoints = npoints; turn = turn } as board) = 
     let newboard = 
-        match (((n > 0 && n < 7) && board.turn = South) || ((n > 5 && n < 13) && board.turn = North)) && ((getSeeds n board) > 0) with
-            |true -> match n with 
-                |1 -> sowseeds board.b board.c board.d board.e board.f board.a' board.b' board.c' board.d' board.e' board.f' 0 board.a 1 1
-                |2 -> sowseeds board.c board.d board.e board.f board.a' board.b' board.c' board.d' board.e' board.f' board.a 0 board.b 2 1 
-                |3 -> sowseeds board.d board.e board.f board.a' board.b' board.c' board.d' board.e' board.f' board.a board.b 0 board.c 3 1
-                |4 -> sowseeds board.e board.f board.a' board.b' board.c' board.d' board.e' board.f' board.a board.b board.c 0 board.d 4 1
-                |5 -> sowseeds board.f board.a' board.b' board.c' board.d' board.e' board.f' board.a board.b board.c board.d 0 board.e 5 1
-                |6 -> sowseeds board.a' board.b' board.c' board.d' board.e' board.f' board.a board.b board.c board.d board.e 0 board.f 6 1
-                |7 -> sowseeds board.b' board.c' board.d' board.e' board.f' board.a board.b board.c board.d board.e board.f 0 board.a' 7 1
-                |8 -> sowseeds board.c' board.d' board.e' board.f' board.a board.b board.c board.d board.e board.f board.a' 0 board.b' 8 1
-                |9 -> sowseeds board.d' board.e' board.f' board.a board.b board.c board.d board.e board.f board.a' board.b' 0 board.c' 9 1
-                |10 -> sowseeds board.e' board.f' board.a board.b board.c board.d board.e board.f board.a' board.b' board.c' 0 board.d' 10 1
-                |11 -> sowseeds board.f' board.a board.b board.c board.d board.e board.f board.a' board.b' board.c' board.d' 0 board.e' 11 1
-                |12 -> sowseeds board.a board.b board.c board.d board.e board.f board.a' board.b' board.c' board.d' board.e' 0 board.f' 0 1
-            |false -> board
-    match pHasNoSeeds newboard with
-        |true -> board
-        |false -> newboard
+        let newboard =
+            match (houseBelongsToPlayer n board) && (houseIsNotEmpty n board) with
+                |true -> match n with 
+                    |1 -> sowseeds (b, c, d, e, f, a', b', c', d', e', f', 0, spoints, npoints, turn, 1) a 1
+                    |2 -> sowseeds (c, d, e, f, a', b', c', d', e', f', a, 0, spoints, npoints, turn, 2) b 1 
+                    |3 -> sowseeds (d, e, f, a', b', c', d', e', f', a, b, 0, spoints, npoints, turn, 3) c 1
+                    |4 -> sowseeds (e, f, a', b', c', d', e', f', a, b, c, 0, spoints, npoints, turn, 4) d 1
+                    |5 -> sowseeds (f, a', b', c', d', e', f', a, b, c, d, 0, spoints, npoints, turn, 5) e 1
+                    |6 -> sowseeds (a', b', c', d', e', f', a, b, c, d, e, 0, spoints, npoints, turn, 6) f 1
+                    |7 -> sowseeds (b', c', d', e', f', a, b, c, d, e, f, 0, spoints, npoints, turn, 7) a' 1
+                    |8 -> sowseeds (c', d', e', f', a, b, c, d, e, f, a', 0, spoints, npoints, turn, 8) b' 1
+                    |9 -> sowseeds (d', e', f', a, b, c, d, e, f, a', b', 0, spoints, npoints, turn, 9) c' 1
+                    |10 -> sowseeds (e', f', a, b, c, d, e, f, a', b', c', 0, spoints, npoints, turn, 10) d' 1
+                    |11 -> sowseeds (f', a, b, c, d, e, f, a', b', c', d', 0, spoints, npoints, turn, 11) e' 1
+                    |12 -> sowseeds (a, b, c, d, e, f, a', b', c', d', e', 0, spoints, npoints, turn, 0) f' 1
+                    |_ -> board
+                |false -> board
+        match pHasNoSeeds newboard with
+            |true -> board
+            |false -> newboard
+    newboard
 //UseHouse
 
 let start position = {a = 4; b = 4; c = 4; d = 4; e = 4; f = 4; a' = 4; b' = 4; c' = 4; d' = 4; e' = 4; f' = 4; npoints = 0; spoints = 0; turn = position}
